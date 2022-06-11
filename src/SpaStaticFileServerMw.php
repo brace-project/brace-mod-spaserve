621a1545fb536595d63ce80f1a63b7146152b7d7
Matthias <m@tth.es>
2022-06-11 16:15:38 +0200

    rename

--- src/SpaStaticFileServerMw.php
+++ src/SpaStaticFileServerMw.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
-class FileServeMw extends BraceAbstractMiddleware
+class SpaStaticFileServerMw extends BraceAbstractMiddleware
 {
 
     const MIME_MAP = [
