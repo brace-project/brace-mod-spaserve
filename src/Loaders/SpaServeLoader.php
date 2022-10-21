2d44bc07587a26c07b513451a8bbd62262309ebe
Matthias Leuffen <matthes@leuffen.de>
2022-10-22 00:27:14 +0200

    update

--- src/Loaders/SpaServeLoader.php
+++ src/Loaders/SpaServeLoader.php
@@ -2,9 +2,11 @@
 
 namespace Brace\SpaServe\Loaders;
 
+use Brace\SpaServe\SpaStaticFileServerMw;
+
 interface SpaServeLoader
 {
     public function matchesRoute(string $route) : bool;
 
-    public function getResponse(string $route) : ServerResponse;
+    public function getResponse(string $route, SpaStaticFileServerMw $middleware) : ServerResponse;
 }
